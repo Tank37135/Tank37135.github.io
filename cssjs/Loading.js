bfce6d20ecb27766adc0cecc73019c63715499b0
Tank37135 <160022209+Tank37135@users.noreply.github.com>
2024-11-04 08:05:55 +0800

    Add files via upload

--- cssjs/Loading.js
+++ cssjs/Loading.js
@@ -8,3 +8,14 @@
             document.body.classList.remove('loading'); // 移除加载类
             document.body.style.overflow = 'auto'; // 允许滚动
         });
+        document.querySelectorAll('.lazy').forEach(image => {
+    image.addEventListener('load', () => {
+        const container = image.parentElement; // 获取图片的父容器
+        const loader = container.querySelector('.loader'); // 查找加载动画
+        loader.style.display = 'none'; // 隐藏加载动画
+        image.style.opacity = 1; // 显示图片
+    });
+
+    // 添加默认样式，使图片初始不可见
+    image.style.opacity = 0; // 初始设置为透明
+});
